b56bb66fa37e6f623a3aa05267a715d2432d4896
darkelf007 <120393859+darkelf007@users.noreply.github.com>
2023-09-24 18:27:25 +0300

    First part of the task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На сколько человек разделить счет?");
+        while (true){
+            int persons = scanner.nextInt();
+            if ( persons <=1 ) {
+                System.out.println("Не корректоное значение для подсчета");
+            }
+            if (persons>1){
+                break;
+            }
+        }
     }
 }
